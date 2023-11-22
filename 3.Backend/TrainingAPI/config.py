9975e60c069d0bbe76fafed3c55ea125cae90bc2
trunghieu <trunghieu9102002@gmail.com>
2023-11-22 09:02:35 +0700

    modify config

--- 3.Backend/TrainingAPI/config.py
+++ 3.Backend/TrainingAPI/config.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from textwrap import dedent
 
 load_dotenv()
 
@@ -15,12 +16,31 @@ class Config:
         'workers': 1
     }
     # uWSGI를 통해 배포되어야 하므로, production level에선 run setting을 건드리지 않음
+    FALLBACK_ERROR_FORMAT = 'json'
+
+    OAS_UI_DEFAULT = 'swagger'
+    SWAGGER_UI_CONFIGURATION = {
+        'apisSorter': "alpha",
+        'docExpansion': "list",
+        'operationsSorter': "alpha"
+    }
 
     SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
     EXPIRATION_JWT = 3600  # seconds
 
-    REDIS = 'redis://localhost:6379/0'
+    REDIS = 'redis://:trunghieu0910@0.0.0.0:6379/0'
 
+    API_HOST = os.getenv('API_HOST', '0.0.0.0:8081')
+    API_SCHEMES = os.getenv('API_SCHEMES', 'http')
+    API_VERSION = os.getenv('API_VERSION', '0.1.0')
+    API_TITLE = os.getenv('API_TITLE', 'Demo API')
+    API_CONTACT_EMAIL = os.getenv('API_CONTACT_EMAIL', 'example@gmail.com')
+
+    API_DESCRIPTION = os.getenv('API_DESCRIPTION', dedent(
+        """
+        ## Explore the API
+        """
+    ))
 
 class LocalDBConfig:
     pass
